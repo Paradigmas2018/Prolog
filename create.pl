a7a103508c9286c61c712c3e1bbc85b576ff43bb
Vitor Menezes <menezesvitorg@gmail.com>
2018-10-02 17:11:05 -0300

    Removed useless code in add station edge rule base case

--- create.pl
+++ create.pl
@@ -4,14 +4,14 @@
 
 
 % add edges for each couple station which are connected on the line
-add_station_edge([Head|[]]) :- last([Head|[]], Last), Head == Last.
+add_station_edge([_|[]]).
 add_station_edge([[Station|_], [Station_2|Distance]|Tail]) :-
   add_edge(metro, Station, Station_2, Distance),
   add_station_edge([[Station_2|Distance]|Tail]).
 
 /* same work from above, but when the line is reversed, ie way back
   this done to get the proper distance for the connection */
-add_station_edge_reverse([Head|[]]) :- last([Head|[]], Last), Head == Last.
+add_station_edge_reverse([_|[]]).
 add_station_edge_reverse([[Station|Distance], [Station_2|Distance2]|Tail]) :-
     add_edge(metro, Station, Station_2, Distance),
     add_station_edge_reverse([[Station_2|Distance2]|Tail]).
