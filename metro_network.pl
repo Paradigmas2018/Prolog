4f49a91b934ffe438af20fbf5352ec6dac752719
Vitor Menezes <menezesvitorg@gmail.com>
2018-10-02 15:07:12 -0300

    Added lines 1 and 2 metro stations

---
+++ metro_network.pl
@@ -0,0 +1,51 @@
+% let train speed be 9 m/s
+
+train('line 1',
+  [
+    ['gare de l\'ouest', 0],
+    ['beekkant', 540],
+    ['etangs noirs', 780],
+    ['comte de flandre', 580],
+    ['sainte-catherine', 610],
+    ['de brouckere', 350],
+    ['gare centrale', 670],
+    ['parc', 410],
+    ['arts-loi', 450],
+    ['maelbeek', 580],
+    ['schuman', 390],
+    ['merode', 1190],
+    ['montgomery', 670],
+    ['josephine-charlotte', 480],
+    ['gribaumont', 430],
+    ['tomberg', 520],
+    ['roodebeek', 890],
+    ['vandervelde', 880],
+    ['alma', 2090],
+    ['crainheim', 1960],
+    ['stockel', 940]
+  ]
+).
+
+train('line 2',
+  [
+    ['simonis', 0],
+    ['osseghem', 780],
+    ['beekkant', 400],
+    ['gare de l\'ouest', 540],
+    ['delacroix', 500],
+    ['clemenceau', 610],
+    ['gare du midi', 740],
+    ['porte de hal', 600],
+    ['hotel des monnaies', 430],
+    ['louise', 470],
+    ['porte de namur', 590],
+    ['trone', 430],
+    ['arts-loi', 520],
+    ['madou', 490],
+    ['botanique', 460],
+    ['rogier', 530],
+    ['yser', 450],
+    ['ribaucourt', 1120],
+    ['elisabeth', 580]
+  ]
+).
